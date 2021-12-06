b46428837264ae17cb3f523069b3bf97a42c18b6
Leonardo Schneider <lnrdschneider@gmail.com>
2021-12-06 14:46:49 -0300

    add and remove asterisk functions added

--- js.js
+++ js.js
@@ -2,3 +2,15 @@ function numberGen(clicked_id) {
   var storedValue = document.getElementById(clicked_id).innerText;
   console.log(storedValue);
 }
+
+let count = '';
+function addAsterisk() {
+  count += '*';
+  document.getElementById('demo').innerHTML = count;
+  console.log(count);
+}
+
+function removeAsterisk() {
+  count = count.slice(0, -1);
+  document.getElementById('demo').innerHTML = count;
+}
