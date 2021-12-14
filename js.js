5745e724706c0332527fd62a967b18e0b6fb5836
Leonardo Schneider <lnrdschneider@gmail.com>
2021-12-14 16:31:42 -0300

    generic values created and payWithCard function created

--- js.js
+++ js.js
@@ -19,14 +19,26 @@ function removeAsterisk() {
 function cancelAsterisk() {
   count = '';
   document.getElementById('pwScreen').innerHTML = count;
+  document.getElementById('pwScreen').innerHTML = '';
+  document.getElementById('value').innerHTML = '';
   console.log(count);
 }
 
+function payWithCard(clicked_id) {
+  var storedValue = document.getElementById(clicked_id).innerText;
+  if (storedValue <= 6) {
+    document.getElementById('value').innerHTML = 'Value ¥' + storedValue * 100;
+  } else {
+    document.getElementById('value').innerHTML = 'Value ¥' + storedValue * 40;
+  }
+  document.getElementById('pwScreen').innerHTML = 'Password:';
+}
+
 function enterPassword() {
-  document.getElementById('value').innerHTML = '';
   document.getElementById('pwScreen').innerHTML = 'Approved!';
   setTimeout(() => {
     document.getElementById('pwScreen').innerHTML = '';
+    document.getElementById('value').innerHTML = '';
   }, 3000);
   console.log(count);
 }
